7f12f9b18bae2d818931a675f54de5c97b57955d
liuhao <1984779164@qq.com>
2022-10-24 17:26:54 +0800

    style: iconfont 目录迁移

--- src/utils/iconfontAdapter.ts
+++ src/utils/iconfontAdapter.ts
@@ -3,21 +3,21 @@
  * @Author: 枫
  * @LastEditors: 枫
  * @description: IconFont 适配器
- * @LastEditTime: 2022-06-21 21:45:09
+ * @LastEditTime: 2022-10-24 17:24:56
  */
 import { Icon } from "@arco-design/web-vue";
 
 // 表情图标
 export const FacialIconFont = Icon.addFromIconFontCn({
-  src: "/src/assets/facial/iconfont.js",
+  src: "/iconfont/facial/iconfont.js",
 });
 
 // 礼物图标
 export const PresentIconFont = Icon.addFromIconFontCn({
-  src: "/src/assets/present/iconfont.js",
+  src: "/iconfont/present/iconfont.js",
 });
 
 // 通用图标
 export const CommonIconFont = Icon.addFromIconFontCn({
-  src: "/src/assets/common/iconfont.js",
+  src: "/iconfont/common/iconfont.js",
 });
